6921c048bf09c815416f5d3bc77235659c781af6
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-11-05 12:43:03 +0100

    fix staatsoper

--- ticketswatcher/shops/staatsoper.py
+++ ticketswatcher/shops/staatsoper.py
@@ -25,11 +25,11 @@ def getConcerts():
     year = datetime.now().year
     month = datetime.now().month
     url_dates = [f"{year + i // 12}-{i % 12 + 1:02}" for i in range(month - 1, month + 11)]
-    spielplan_urls = [f"https://www.staatsoper.de/spielplan/{year_month}" for year_month in url_dates]
+    spielplan_urls = [f"https://www.staatsoper.de/spielplan/{year_month}/calendar.ajax" for year_month in url_dates]
 
     concerts = []
     for url in spielplan_urls:
-
+        print(f"fetching concerts from {url}")
         concerts += parse_spielplan_page(url)
 
     return concerts
@@ -40,7 +40,10 @@ def parse_spielplan_page(url):
     r = requests.get(url, cookies=COOKIES, headers=HEADERS)
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    for concert in soup.find_all(class_='activity-list__content'):
+    for concert in soup.find_all(class_='activity-list__row'):
+        if "In diesem Zeitraum gibt es keine Vorstellungen" in concert.select_one('h3').text:
+            continue
+
         date = try_get_attribute(concert, 'time', 'datetime')
         title = try_get_text(concert, '.activity-list__text h3')
 
@@ -48,13 +51,17 @@ def parse_spielplan_page(url):
         date_time = datetime.strptime(date + ' ' + time.strip(), '%Y-%m-%d %H.%M Uhr')
         venue = location.strip()
 
-        url = try_get_attribute(concert, '.activity-list__content', 'href')
-        ticket_url = try_get_attribute(concert, '.button .button--ticket', 'href')
+        base_url = 'https://staatsoper.de'
+        url = base_url + try_get_attribute(concert, '.activity-list__content', 'href')
+        ticket_url = try_get_attribute(concert, '.activity-list__col--tickets .button--ticket', 'href')
+
+        category = try_get_text(concert, '.activity-list__col--genre').strip()
 
         concerts.append({
             'provider': 'staatsoper',
             'title': title.strip(),
             'details': try_get_text(concert, '.activity-list--toggle__content'),
+            'category': category,
             'datestr': '',
             'datetime': date_time,
             'image': '',
