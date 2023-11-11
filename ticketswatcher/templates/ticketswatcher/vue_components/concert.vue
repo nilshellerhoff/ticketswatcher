084ec1070cb95eaf056b905947a1ba62bbf608d1
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-11-11 09:42:34 +0100

    refactor: move to vue components

---
+++ ticketswatcher/templates/ticketswatcher/vue_components/concert.vue
@@ -0,0 +1,34 @@
+<template id="templateConcert">
+  <a :href="`/concert/${concert.pk}`" class="text-decoration-none">
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+      <div class="ms-2 me-auto">
+        [[ concert.fields.provider_title ]]
+        <div class="fw-bold">[[ concert.fields.title ]]</div>
+        <span style="font-size: 14px">
+                    <i class="bi bi-clock-fill"></i>
+                      <formatted-date :date="concert.fields.datetime"/>
+                    <br>
+                    <i class="bi bi-geo-alt-fill"></i> [[ concert.fields.venue ]]
+                </span>
+      </div>
+      <span :class="`badge ${availablePill.color} rounded-pill`">[[ availablePill.label ]]</span>
+    </li>
+  </a>
+</template>
+
+<script>
+Vue.component('concert', {
+  delimiters: ['[[', ']]'],
+  template: '#templateConcert',
+  props: ['concert'],
+  computed: {
+    availablePill() {
+      const avail = this.concert.fields.available_tickets
+      if (avail >= 20) return {label: '20+', color: 'bg-success'}
+      else if (avail > 0) return {label: avail, color: 'bg-warning'}
+      else if (avail !== null) return {label: avail, color: 'bg-danger'}
+      else return {label: '--', color: 'bg-secondary'}
+    },
+  }
+});
+</script>
---
+++ ticketswatcher/templates/ticketswatcher/vue_components/formatted_date.vue
@@ -0,0 +1,18 @@
+<template id="templateFormattedDate">
+  <span>[[ formattedDate ]]</span>
+</template>
+
+<script>
+    Vue.component('formattedDate', {
+        delimiters: ['[[', ']]'],
+        template: '#templateFormattedDate',
+        props: ['date'],
+        computed: {
+            formattedDate() {
+                const datetime = new Date(this.date)
+                const dateOptions = {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'}
+                return datetime.toLocaleString('de-DE', dateOptions)
+            }
+        }
+    });
+</script>
