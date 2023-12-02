660507971359e4b8c3302b7c9686c707044c5a70
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-12-02 18:03:44 +0100

    use vuetify table

--- ticketswatcher/templatetags/variable.py
+++ ticketswatcher/templatetags/variable.py
@@ -1,5 +1,6 @@
 import json
 from django import template
+from django.db.models import QuerySet
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -7,6 +8,9 @@ register = template.Library()
 @register.filter
 
 def variable(value, name):
+    if type(value) == QuerySet:
+        value = list(value)
+
     json_str = json.dumps(value)
     # second encoding encodes quotes
     js_str = json.dumps(json_str)
