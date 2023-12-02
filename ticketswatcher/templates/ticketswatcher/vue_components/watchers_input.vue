35e5173e7b54121e09274b574abbf71c74cb502a
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-12-02 12:47:32 +0100

    validation on watcher input

---
+++ ticketswatcher/templates/ticketswatcher/vue_components/watchers_input.vue
@@ -0,0 +1,40 @@
+<template id="templateWatchersInput">
+  <form>
+    <v-row>
+      <v-text-field
+          v-model="email"
+          clearable
+          label="Email"
+          prepend-inner-icon="mdi-at"
+          variant="solo"
+          name="email"
+          type="email"
+          :rules="emailRules"
+      >
+        <template v-slot:append>
+          <v-btn
+              type="submit"
+              color="primary"
+          >Show
+          </v-btn>
+        </template>
+      </v-text-field>
+    </v-row>
+  </form>
+</template>
+
+<script>
+const watchersInput = {
+  delimiters: ['[[', ']]'],
+  template: '#templateWatchersInput',
+  props: ['defaultEmail'],
+  data() {
+    return {
+      email: this.defaultEmail,
+      emailRules: [v => /^[^\s@]+@[^\s@]+$/.test(v) || 'Please enter an email address']
+    }
+  },
+}
+
+app.component('watchersInput', watchersInput);
+</script>
